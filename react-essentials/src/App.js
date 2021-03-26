90436a3361eb66bd45d8483313fb91f93d7bae55
Combatd <combatdstar@gmail.com>
2021-03-26 13:16:53 -0700

    Create Main component

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+
 import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <h2>Main</h2>
+      <Main />
       <h3>Footer</h3>
     </div>
   );
---
+++ react-essentials/src/Components/Main/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function main() {
+    return (
+        <section>
+            <p>We serve the most delicious food around.</p>
+        </section>
+    )
+}
