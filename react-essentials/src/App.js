5eebf00f0b426ded7e5e65a4477d1a7e1449baea
Combatd <combatdstar@gmail.com>
2021-03-26 13:50:08 -0700

    Send props.dishes to Main component

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -5,11 +5,17 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
 
+const dishes = [
+  'Macaroni and Cheese',
+  'Garlic Butter Salmon',
+  'Steak and Potatoes'
+]
+
 function App() {
   return (
     <div className="App">
       <Header name="Cindy" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishes} />
       <Footer year={ new Date().getFullYear() } />
     </div>
   );
--- react-essentials/src/Components/Main/index.js
+++ react-essentials/src/Components/Main/index.js
@@ -4,6 +4,11 @@ export default function main(props) {
     return (
         <section>
             <p>We serve the most {props.adjective} food around.</p>
+            <ul  style={ {textAlign: 'left'} }>
+                { props.dishes.map((dish) => {
+                    return <li> {dish} </li>
+                }) }
+            </ul>
         </section>
     )
 }
