854e5c1bc31bfcb891d78d2fc88251f5e5f0371d
Combatd <combatdstar@gmail.com>
2021-03-27 15:51:56 -0700

    Handle errors  in fetch using promises

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -12,12 +12,21 @@ function App({ login }) {
   // const [checked, toggle] = useReducer((checked) => !checked, false);
 
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect( () => {
+    if (!login) {
+      return;
+    }
+    setLoading(true);
+
     fetch('https://api.github.com/users/' + login)
     .then( (res) => res.json())
     .then(setData)
-  }, []);
+    .then( () => setLoading(false))
+    .catch(setError);
+  }, [login]);
 
   // useEffect(() => {
   //   console.log(`It's ${secondary} around here!`);
@@ -47,21 +56,28 @@ function App({ login }) {
   //   setSecondary('Placid');
   // }
 
-  if (data) {
-    return (
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    <pre>
+      {JSON.stringify(error, null, 2)}
+    </pre>
+  }
+
+  if(!data) {
+    return null;
+  }
+
+  return (
     <div>
       <h1>{data.name}</h1>
       <h2>{data.location}</h2>
       <img alt={data.login} src={data.avatar_url} />
     </div>
     );
-  }
 
-  return (
-    <div>
-      No User Available
-    </div>
-  );
 }
 
 export default App;
